7e958cbf1e97b53ee663f33392f941559e491153
Mike Pennisi <mike@mikepennisi.com>
2013-12-14 16:20:15 -0500

    Abstract data dereferencing
    In order to better facilitate reusability, provide an interface for
    working with chart data. With it, chart consumers are able to define the
    "shape" of their data. Allow consumers to explicitly opt out of this
    behavior for situations that require runtime optimization (i.e. large
    datasets, frequently-updating charts, or low-powered devices).

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,9 @@ module.exports = function(grunt) {
         "src/layer-extensions.js",
         "src/chart.js",
         "src/chart-extensions.js"
+      ],
+      libraries: [
+        "node_modules/datamap/src/datamap.js"
       ]
     },
     watch: {
@@ -34,6 +37,7 @@ module.exports = function(grunt) {
             d3Chart: true,
             Layer: true,
             Chart: true,
+            DataMap: true,
             variadicNew: true
           }
         },
@@ -46,8 +50,8 @@ module.exports = function(grunt) {
           globals: {
             d3: true,
             assert: true,
-            chai: true,
             setup: true,
+            suiteSetup: true,
             teardown: true,
             suite: true,
             test: true,
@@ -83,7 +87,7 @@ module.exports = function(grunt) {
       },
       release: {
         files: {
-          "d3.chart.js": "<%= meta.srcFiles %>"
+          "d3.chart.js": ["<%= meta.libraries %>", "<%= meta.srcFiles %>"]
         }
       }
     },
